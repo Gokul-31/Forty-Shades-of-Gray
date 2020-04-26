cf9d47099ad791ff073e25e7dfbd8e9548afc911
Gokul-31 <63341335+Gokul-31@users.noreply.github.com>
2020-04-26 13:11:19 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,17 +2,19 @@ let numArray = [];
 let items;
 var num;
 let numCheck = 0;
-let timeElapsed=0;
+let timeElapsed = 0;
 // var timeBlock;
 let min = 0;
-var sec;
-var msec;
+var sec=0;
+var msec=0;
 var TimerInterval;
 
-let timeBlockTime = document.getElementById('timeBlockTime');
+let timeBlockTime = document.getElementsByClassName('timeBlockTime');
 
 items = document.getElementsByClassName('item');
 // timeBlock = document.getElementById('timeBlock');
+
+initiate();
 
 for (let i = 0; i < 20; i++) {
   numArray[i] = i + 1;
@@ -22,7 +24,7 @@ var bigDiv = document.getElementById('grid');
 for (let i = 0; i < 20; i++) {
   var insideDiv = document.createElement('div');
   var p = document.createElement('p');
-  p.innerHTML = "" + numArray[i];
+  p.textContent = "" + numArray[i];
   insideDiv.appendChild(p);
   insideDiv.classList.add('item')
   bigDiv.appendChild(insideDiv);
@@ -32,20 +34,19 @@ for (let i = 0; i < 20; i++) {
   items[i].addEventListener("mouseover", function update_item() {
     num = parseInt(items[i].textContent);
     if (num == numCheck + 1) {
-      if (num == 1) {
-        timeElapsed =0;
-        startTimer();
+      if (num === 1) {
+        timeElapsed = 0;
       }
       if (num <= 20) {
         num += 20;
-        items[i].innerHTML = "" + num;
+        items[i].textContent = "" + num;
       }
       else if (num > 20 && num < 40) {
-        items[i].innerHTML = "";
+        items[i].textContent = "";
       }
-      else if (num == 40) {
-        items[i].innerHTML = "";
-        clearInterval(Timer);
+      else if (num === 40) {
+        items[i].textContent = "";
+        clearInterval(TimerInterval);
       }
       numCheck++;
       //display the items properly
@@ -54,11 +55,12 @@ for (let i = 0; i < 20; i++) {
   });
 }
 
-function startTimer(){
-  TimerInterval=setInterval(Timer(),1);
+function initiate() {
+  //initiate countdown timer
+  TimerInterval = setInterval(Timer, 1);
 }
 
-function Timer(){
+function Timer() {
   timeElapsed++;
   sec = Math.floor(timeElapsed / 1000);
   if (sec >= 60) {
@@ -66,25 +68,19 @@ function Timer(){
     sec %= 60;
   }
   msec = timeElapsed % 1000;
-  if (min != 0) {
-    timeBlockTime.innerHTML = min + "m :";
-    if (sec < 10) {
-      timeBlockTime.innerHTML += "0";
-    }
-    else {
-      timeBlockTime.innerHTML = "0m :";
-      if (sec < 10) {
-        timeBlockTime.innerHTML += "0";
-      }
-    }
-    timeBlockTime.innerHTML += sec + "s :";
-    if (msec < 10) {
-      timeBlockTime.innerHTML += "00";
-    }
-    else if (msec < 100) {
-      timeBlockTime.innerHTML += "0";
-    }
-    timeBlockTime.innerHTML += msec + "ms";
+  
+  timeBlockTime.textContent= min+"m : ";
+  if(sec<10){
+    timeBlockTime.textContent+="0";
   }
+  timeBlockTime.textContent += sec + "s : ";
+  if (msec < 10) {
+    timeBlockTime.textContent += "00";
+  }
+  else if (msec < 100) {
+    timeBlockTime.textContent += "0";
+  }
+  timeBlockTime.textContent += msec + "ms";
 
-  }
+
+}
